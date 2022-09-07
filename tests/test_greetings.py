0a8a33cf2ba9544cad8c891d7b17b144716ab36d
Juan Fernando Parra Cadavid <jparrax17@gmail.com>
2022-09-07 18:23:07 +1200

    Update greetins.py
    - Add function say_welcome

--- tests/test_greetings.py
+++ tests/test_greetings.py
@@ -1,6 +1,6 @@
 """This is for checking github action for tests"""
 import unittest
-from src.greetings import say_hello
+from src.greetings import say_hello, say_welcome
 
 class TestGreetings(unittest.TestCase):
     """Test class"""
@@ -8,6 +8,10 @@ class TestGreetings(unittest.TestCase):
     def test_say_hello(self):
         """Let's check if say_hello actually say Hello"""
         self.assertEqual(say_hello(), 'Hello')
+
+    def test_say_welcome(self):
+        """Let's check if say_welcome actually say Welcome"""
+        self.assertEqual(say_welcome(), 'Welcome')
 
 if __name__ == '__main__':
     unittest.main()
