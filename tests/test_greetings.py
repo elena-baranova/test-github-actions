9f400890140b7663bd7ee7a47fe50999acfe5690
elena <elena.baranova@danone.com>
2022-09-08 10:18:25 +1200

    Update test_greetings.py
    Fix bug

--- tests/test_greetings.py
+++ tests/test_greetings.py
@@ -2,16 +2,18 @@
 import unittest
 from src.greetings import say_hello, say_welcome
 
+
 class TestGreetings(unittest.TestCase):
     """Test class"""
 
     def test_say_hello(self):
         """Let's check if say_hello actually say Hello"""
-        self.assertEqual(say_hello(), 'Hello1')
+        self.assertEqual(say_hello(), "Hello")
 
     def test_say_welcome(self):
         """Let's check if say_welcome actually say Welcome"""
-        self.assertEqual(say_welcome(), 'Welcome')
+        self.assertEqual(say_welcome(), "Welcome")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
