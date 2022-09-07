4d516c1bb7be9f8fb61ee5199d099f6019dc9c97
Juan Fernando Parra Cadavid <jparrax17@gmail.com>
2022-09-07 16:26:55 +1200

    Fix lint

--- src/greetings.py
+++ src/greetings.py
@@ -1,2 +1,4 @@
+"""This is my test"""
 def say_hello():
+    """This is my test function"""
     return 'Hello'
--- tests/test_greetings.py
+++ tests/test_greetings.py
@@ -1,9 +1,12 @@
+"""This is for checking github action for tests"""
 import unittest
 from src.greetings import say_hello
 
 class TestGreetings(unittest.TestCase):
+    """Test class"""
 
     def test_say_hello(self):
+        """Let's check if say_hello actually say Hello"""
         self.assertEqual(say_hello(), 'Hello')
 
 if __name__ == '__main__':
